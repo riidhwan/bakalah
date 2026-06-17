b430a7b55b57929ced554133576f18902f638b07
riidhwan <ridhwan@ramdhani.me>
2026-06-17 18:28:57 +0200

    Prepare 0.30.0 release (#167)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,8 @@ import mihon.gradle.getLatestCommitSha
 import mihon.gradle.tasks.ReplaceShortcutsPlaceholderTask
 
 @Suppress("MagicNumber")
-private val bakalahVersionCode = 43
-private val bakalahVersionName = "0.29.1"
+private val bakalahVersionCode = 44
+private val bakalahVersionName = "0.30.0"
 
 plugins {
     alias(mihonx.plugins.android.application)
